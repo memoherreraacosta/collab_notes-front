9a3ddd8e39c2aea6f79e6f750d714696ed74cb7a
Karol José Gutiérrez Suárez <kjgs.22@gmail.com>
2020-04-13 00:44:05 -0500

    Add MarkDown Editor

--- src/views/NoteEditor.jsx
+++ src/views/NoteEditor.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import ReactMde from "react-mde";
+import ReactDOM from "react-dom";
+import * as Showdown from "showdown";
+import "react-mde/lib/styles/css/react-mde-all.css";
+
+const converter = new Showdown.Converter({
+    tables: true,
+    simplifiedAutoLink: true,
+    strikethrough: true,
+    tasklists: true
+  });
 
 const NoteEditor = () => {
+    const [value, setValue] = React.useState("**Hello world!!!**");
+    const [selectedTab, setSelectedTab] = React.useState("write");
     return (
-        <>
-            This is the the Note Editor.
-        </>
+        <div className="container">
+            <ReactMde
+                value={value}
+                onChange={setValue}
+                selectedTab={selectedTab}
+                onTabChange={setSelectedTab}
+                generateMarkdownPreview={markdown =>
+                    Promise.resolve(converter.makeHtml(markdown))
+                }
+            />
+        </div>
     );
 };
 
