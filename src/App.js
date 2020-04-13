404edd28660a2b59dfa2a30036d417a739393aa6
Karol José Gutiérrez Suárez <kjgs.22@gmail.com>
2020-04-12 20:24:13 -0500

    Change folder structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import NavBar from "./components/NavBar";
-import { useAuth0 } from "./react-auth0-spa";
+import { useAuth0 } from "./auth/react-auth0-spa";
 
 function App() {
   const { loading } = useAuth0();
