a66ff172aefe85ff481d95d56bbe1103e906f5c3
Karol José Gutiérrez Suárez <kjgs.22@gmail.com>
2020-04-13 00:04:58 -0500

    Add Node Editor empty template

---
+++ src/views/NoteEditor.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Loading from "../components/Loading";
+import { useAuth0 } from "../auth/react-auth0-spa";
+
+const NoteEditor = () => {
+    return (
+        <>
+            This is the the Note Editor.
+        </>
+    );
+};
+
+export default NoteEditor;
