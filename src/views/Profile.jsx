10946438df552059c498b88968de2dffff3df0d3
Karol José Gutiérrez Suárez <kjgs.22@gmail.com>
2020-04-15 13:27:28 -0500

    Add profile page

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,12 +1,47 @@
 import React from "react";
-import Loading from "../components/Loading";
 import { useAuth0 } from "../auth/react-auth0-spa";
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 const Profile = () => {
+    const { user } = useAuth0();
     return (
-        <>
-            This is the profile.
-        </>
+        <Form>
+            <FormGroup>
+                <img
+                    src={user.picture}
+                    alt="Profile"
+                    className="nav-user-profile rounded-circle"
+                    width="20%"
+                />
+                <h1>{user.name}</h1>
+            </FormGroup>
+            <br/>
+            <FormGroup>
+                <Label>Email: </Label>
+                <p>{user.email}</p>
+            </FormGroup>
+            <br/>
+            <FormGroup>
+                <Label>School: </Label>
+                <p>Tecnológico de Monterrey Campus Guadalajara</p>
+            </FormGroup>
+            <br/>
+            <FormGroup>
+                <Label>About me: </Label>
+                <p>I'm a senior Computer Science student.</p>
+            </FormGroup>
+            <br/>
+            <FormGroup>
+                <Label>My courses: </Label>
+                <ul>
+                    <li>Web development</li>
+                    <li>Mobile development</li>
+                    <li>Cuantitative methods</li>
+                    <li>Data Science</li>
+                    <li>Intelligent Systems</li>
+                </ul>
+            </FormGroup>  
+    </Form>
     );
 };
 
