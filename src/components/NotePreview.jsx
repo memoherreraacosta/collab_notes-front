be131145267f62cc0dafed67563433c10775b8b4
Karol José Gutiérrez Suárez <kjgs.22@gmail.com>
2020-05-06 17:58:03 -0500

    Change text in NoteView

--- src/components/NotePreview.jsx
+++ src/components/NotePreview.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import { Jumbotron, Button } from 'reactstrap';
 
 const NotePreview = (props) => {
+
+
   return (
     <div>
       <Jumbotron>
         <h1 className="display-6">{props.subject}</h1>
-        <p className="lead">This is the abstract of the note</p>
+        <p className="lead onelinenote" >Note 1 of {props.subject}</p>
         <hr className="my-2" />
-        <p>This is the content of the note.</p>
+        <p className="lead onelinenote" >Note 2 of {props.subject}</p>
         <p className="lead">
-          <Button color="primary">See full note</Button>
+          <Button color="primary">See more</Button>
         </p>
       </Jumbotron>
     </div>
