00f2b05bd1bca82c9ee5504beab8b8140d766385
Karol José Gutiérrez Suárez <kjgs.22@gmail.com>
2020-05-06 16:00:31 -0500

    Add dropdown menu and change profile

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -41,10 +41,7 @@ const NavBar = () => {
                 </NavItem>  
                 <NavItem>
                     <NavLink href='/create/'>Create</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href='/profile/'>Profile</NavLink>
-                </NavItem>    
+                </NavItem> 
               </>
             )}
           </Nav>
@@ -52,14 +49,29 @@ const NavBar = () => {
               <Button onClick={() => loginWithRedirect()}>Log in</Button>
             ):(  
               <>
-                <img
-                  src={user.picture}
-                  alt="Profile"
-                  className="nav-user-profile rounded-circle"
-                  width="50"
-                />
-                <NavbarText className="name">{user.name}</NavbarText>
-                <Button onClick={() => logout()}>Log out</Button>
+                  <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                        <img
+                        src={user.picture}
+                        alt="Profile"
+                        className="nav-user-profile rounded-circle"
+                        width="50"
+                      />
+                      <NavbarText className="name">{user.name}</NavbarText>
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                      <DropdownItem href='/profile/'>
+                        Profile
+                      </DropdownItem>
+                      <DropdownItem>
+                        Settings
+                      </DropdownItem>
+                      <DropdownItem divider />
+                      <DropdownItem>
+                        <Button onClick={() => logout()}>Log out</Button>
+                      </DropdownItem>
+                    </DropdownMenu>
+                </UncontrolledDropdown>
             </>              
           )}
         </Collapse>
