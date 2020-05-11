bfbaf04aeaf67aca495e725af57cc90d6323311d
Guillermo Herrera Acosta <a01400835@itesm.mx>
2020-05-11 01:37:19 -0500

    Proof of concept connection_db utility
    Signed-off-by: Guillermo Herrera Acosta <a01400835@itesm.mx>

---
+++ src/utils/connection_db.js
@@ -0,0 +1,30 @@
+const api_gateway = "https://fsxgo3yvyj.execute-api.us-east-1.amazonaws.com/collab-notes/collab-notes?"
+
+export const connection_db = async (query, is_select) => {
+    query = new Buffer(query).toString('base64');
+    var url = is_select
+        ? "select=" + query
+        : "query=" + query 
+
+    url = api_gateway + url
+    try {
+        const data = await fetch(url);
+        return await data.json();
+    }
+    catch (error) {
+        return error.json();
+    }
+}
+
+/* 
+Proof of concept
+
+{data.map(
+    (register, idx) => 
+        <Component 
+            key={i}
+            title={register.title} 
+            author={register.author} 
+            pages={register.pages}
+            freeBookmark={this.localVar.id}/>
+)} */
